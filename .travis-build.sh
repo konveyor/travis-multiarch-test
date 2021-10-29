129ed3ad1a13fdbcbd8423bb809d3362aea183b4
Jason Montleon <jmontleo@redhat.com>
2021-10-29 09:17:22 -0400

    test 1

---
+++ .travis-build.sh
@@ -0,0 +1,6 @@
+export image=quay.io/jmontleon/travis-multiarch-test:latest
+#export ARCH=$(uname -m)
+
+docker build -t ${image} -f Dockerfile .
+docker login quay.io -u "$QUAY_ROBOT" -p $QUAY_TOKEN
+docker push ${image}
