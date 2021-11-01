10379b993c4044f2808c09f1bbbcbedd36a8f716
Jason Montleon <jmontleo@redhat.com>
2021-11-01 11:50:51 -0400

    use an env var to specify the Dockerfile name

--- .travis-build-image.sh
+++ .travis-build-image.sh
@@ -8,6 +8,6 @@ fi
 
 export ARCH=$(uname -m)
 
-docker build -t ${IMAGE}:${TAG}-${ARCH} -f Dockerfile .
+docker build -t ${IMAGE}:${TAG}-${ARCH} -f ${DOCKERFILE} .
 docker login quay.io -u "${QUAY_ROBOT}" -p ${QUAY_TOKEN}
 docker push ${IMAGE}:${TAG}-${ARCH}
