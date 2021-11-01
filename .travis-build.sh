1beb9f1902104f334d08b586baebd5fc85c3d75b
Jason Montleon <jmontleo@redhat.com>
2021-11-01 10:16:17 -0400

    each build boots a separate VM so we need to push each

--- .travis-build.sh
+++ .travis-build.sh
@@ -2,5 +2,5 @@ export image=quay.io/jmontleon/travis-multiarch-test:latest
 export ARCH=$(uname -m)
 
 docker build -t ${image}-${ARCH} -f Dockerfile .
-#docker login quay.io -u "$QUAY_ROBOT" -p $QUAY_TOKEN
-#docker push ${image}
+docker login quay.io -u "$QUAY_ROBOT" -p $QUAY_TOKEN
+docker push ${image}
